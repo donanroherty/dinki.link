822856c4364c21fb8dfd2eef5af9f3f82dadf117
Ronan Doherty <donanroherty@gmail.com>
2020-06-24 23:04:27 +0100

    Added triangle icon, removed unecessary svg files

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -2,10 +2,12 @@ import github from "./github"
 import daynight from "./daynight"
 import engineer from "./engineer"
 import test from "./test"
+import triangle from "./triangle"
 
 export default {
   github,
   daynight,
   engineer,
   test,
+  triangle,
 }
