851c8bb23a37c42d76cf19704fb8c3c0d43719e3
Ronan Doherty <donanroherty@gmail.com>
2020-06-24 22:06:50 +0100

    Added icons

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,7 +1,11 @@
 import github from "./github"
 import daynight from "./daynight"
+import engineer from "./engineer"
+import test from "./test"
 
 export default {
   github,
   daynight,
+  engineer,
+  test,
 }
