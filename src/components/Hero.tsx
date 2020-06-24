d9db406ab06b31fd7ce4fbc9bfb2997b847240d5
Ronan Doherty <donanroherty@gmail.com>
2020-06-24 23:45:28 +0100

    Styled to fill screen with Hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components/macro"
+import Icon from "./Icon"
 
 function Hero() {
   return (
@@ -7,36 +8,59 @@ function Hero() {
       <IllustrationContainer>
         <div>Illustration</div>
       </IllustrationContainer>
+      <TagLine>
+        <em>Make your linki dinki</em>
+      </TagLine>
       <LinkInputWrapper>
         <div>LinkInput</div>
       </LinkInputWrapper>
+      <ScrollButton>
+        <Icon name="triangle" size={25} color="#7db3ff" />
+      </ScrollButton>
     </StyledHero>
   )
 }
 
+const contentMargin = 36
+
 const StyledHero = styled.div`
   display: flex;
   flex-direction: column;
-  height: 500px;
+  align-items: center;
+  height: 100vh;
+  padding: 0 ${contentMargin}px;
+  padding-bottom: 40px;
+  box-sizing: border-box;
+  margin-top: -72px;
 `
 const IllustrationContainer = styled.div`
   height: 100%;
-
+  width: calc(100% + ${contentMargin}px + ${contentMargin}px);
+  margin-left: -${contentMargin}px;
+  margin-right: -${contentMargin}px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const TagLine = styled.div`
+  font-size: 30px;
+  font-weight: 100;
+  color: #4f5257;
+  margin-top: 20px;
+`
+const LinkInputWrapper = styled.div`
+  width: 100%;
+  height: 100px;
+  margin-top: 20px;
   > div {
     background-color: lightblue;
     width: 100%;
     height: 100%;
   }
 `
-const LinkInputWrapper = styled.div`
-  height: 100px;
-  margin-top: auto;
+const ScrollButton = styled.div`
+  margin-top: 42px;
   padding: 20px;
-  > div {
-    background-color: lightblue;
-    width: 100%;
-    height: 100%;
-  }
 `
 
 export default Hero
